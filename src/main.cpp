c18142ee2ad5e407b36a62d536933ce566d4f8df
kiri-nakata <kiri.nakata@mixi.co.jp>
2019-06-17 11:51:53 +0900

    add code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <boost/di.hpp>
+#include <boost/di/extension/injections/extensible_injector.hpp>
+
+namespace di = boost::di;
+
+struct Hoge {
+    Hoge()
+        : value(0)
+    {
+    }
+
+    int value;
+};
+
+void dump(const Hoge& hoge){
+    std::cout << "(0x" << std::hex << (std::intptr_t)(&hoge) << ") value: " << std::dec << hoge.value << std::endl;
+}
+
+void dump(const std::string& name, const Hoge& hoge){
+    std::cout << "[" << name << "]";
+    dump(hoge);
+}
+
+auto model() {
+  return di::make_injector(
+  );
+}
+
+#define DUMP(val) dump(#val, val)
 
 int main(){
-    std::cout << "Hello, world!" << std::endl;
+    auto injector = model();
+
+    auto& hoge1 = injector.create<Hoge&>();
+    hoge1.value = 100;
+    auto& hoge2 = injector.create<Hoge&>();
+
+    DUMP(hoge1);
+    DUMP(hoge2);
+
+	{
+		auto extended_injector = di::make_injector(
+			di::extension::make_extensible(injector)
+			);
+		auto& hoge3 = injector.create<Hoge&>();
+		DUMP(hoge3);
+	}
+
     return 0;
 }
 
